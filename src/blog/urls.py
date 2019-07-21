8c8f463793e33e34808fa0d076abdcdc80d74efc
MoinulHassinaNabil <nnaabill@gmail.com>
2019-07-21 01:00:58 -0700

    created routing for specific if

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import blog,index,post
+from posts.views import blog, index, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',blog),
+    path('blog/',blog, name = 'blog_details'),
     path('index/',index),
-    path('post/',post),
+    path('post/<id>/',post, name = 'post_details'),
 ]
 
 if settings.DEBUG:
