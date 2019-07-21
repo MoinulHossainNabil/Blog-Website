0e6aa8cd32a9ff0d5fb6957c62b1b7e45f4232ec
MoinulHassinaNabil <nnaabill@gmail.com>
2019-07-21 00:56:51 -0700

    added a new attribute view_count to the Posts model

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -25,7 +26,13 @@ class Posts(models.Model):
     thmbnail = models.ImageField()
     comment = models.IntegerField(default=0)
     featured = models.BooleanField()
+    view_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_details', kwargs={
+            'id' : self.id
+        })
     
