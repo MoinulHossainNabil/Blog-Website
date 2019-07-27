3da225e8c4dd0a411448e5140c632ac85454cb0e
MoinulHassinaNabil <nnaabill@gmail.com>
2019-07-26 21:27:26 -0700

    created routing for the searced result page

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,13 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from posts.views import blog, index, post
+from posts.views import blog, index, post, search_for_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',blog, name = 'blog_details'),
     path('index/',index),
     path('post/<id>/',post, name = 'post_details'),
+    path('search_for_post/', search_for_post, name = 'search_for_post'),
 ]
 
 if settings.DEBUG:
