f57ca9c7730e8f87151d73536c769c8c2b542ca7
MoinulHassinaNabil <nnaabill@gmail.com>
2019-07-29 08:14:55 -0700

    registerd the comment model

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Author, Category, Posts
+from .models import Author, Category, Posts, Comments
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Posts)
+admin.site.register(Comments)
