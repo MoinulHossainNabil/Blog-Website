854b04656d3be0af6ffcd9793b0e4da9fadb54ce
MoinulHassinaNabil <nnaabill@gmail.com>
2019-07-29 08:15:28 -0700

    added the comment model

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from tinymce import HTMLField
 
 User = get_user_model()
 
@@ -21,12 +22,15 @@ class Posts(models.Model):
     post_author = models.ForeignKey(Author,on_delete=models.CASCADE)
     title = models.CharField(max_length = 100)
     body = models.TextField()
+    content = HTMLField()
     category = models.ManyToManyField(Category)
     upload_date = models.DateTimeField(auto_now_add=True)
     thmbnail = models.ImageField()
     comment = models.IntegerField(default=0)
     featured = models.BooleanField()
     view_count = models.IntegerField(default=0)
+    previous_post = models.ForeignKey('self', related_name = 'prev', on_delete = models.SET_NULL, blank = True, null = True)
+    next_post = models.ForeignKey('self', related_name = 'next', on_delete = models.SET_NULL, blank = True, null = True)
 
     def __str__(self):
         return self.title
@@ -35,4 +39,19 @@ class Posts(models.Model):
         return reverse('post_details', kwargs={
             'id' : self.id
         })
+
+    @property
+    def get_comments(self):
+        return self.postcomments.all()
+    
+
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, related_name='postcomments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.author)
+
     
