8e0a58806e78810c9de1c55f878c8ac16ec0d2fa
MoinulHassinaNabil <nnaabill@gmail.com>
2019-08-03 22:28:02 -0700

    modified some model form action

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,13 +1,28 @@
 from django import forms
 from .models import Posts
+from tinymce import TinyMCE
 
-class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        'class' : 'form-control',
-        'placeholder' : 'Type your comment',
-        'id' : 'usercomment',
-        'rows' : '4'
-    }))
+class TinyMCEWidget(TinyMCE):
+    def use_required_attribute(self, *args):
+        return False
+    
+class PostForm(forms.ModelForm):
+    content = forms.CharField(
+        widget = TinyMCEWidget(
+            attrs={'required': False, 'cols': 30, 'rows': 10}
+        )
+    )
     class Meta:
         model = Posts
-        fields = ('content',)
+        fields = ('post_author','title', 'body', 'content', 'category', 'thmbnail', 'featured', 'previous_post', 'next_post')
+
+# class CommentForm(forms.ModelForm):
+#     # content = forms.CharField(widget=forms.Textarea(attrs={
+#     #     'class' : 'form-control',
+#     #     'placeholder' : 'Type your comment',
+#     #     'id' : 'usercomment',
+#     #     'rows' : '4'
+#     # }))
+#     class Meta:
+#         model = Posts
+#         fields = ('content',)
