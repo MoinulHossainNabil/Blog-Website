229f3be3bc5887d8af1e70f557cc993f348c85d8
MoinulHassinaNabil <nnaabill@gmail.com>
2019-08-03 22:26:10 -0700

    added url patters for the post update , create , delete and added some urls from djanog all auth

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,15 +3,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from posts.views import blog, index, post, search_for_post
+from posts.views import blog, index, post, search_for_post, post_create, post_delete, post_update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',blog, name = 'blog_details'),
     path('index/',index),
     path('post/<id>/',post, name = 'post_details'),
+    path('create/',post_create, name = 'post_create'),
+    path('post/<id>/update/',post_update, name = 'post_update'),
+    path('post/<id>/delete/',post_delete, name = 'post_delete'),
     path('search_for_post/', search_for_post, name = 'search_for_post'),
     path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
